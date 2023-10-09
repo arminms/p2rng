6547da272b2c836e9c9eaac6eb71886917304bda
Armin Sobhani <arminms@gmail.com>
2023-10-09 14:15:39 -0400

    add copyright header to files

--- include/p2rng/p2rng.hpp
+++ include/p2rng/p2rng.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2023 Armin Sobhani
+// Copyright (c) 2023 Armin Sobhani (https://arminsobhani.ca)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
